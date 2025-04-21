fc8f287477c97e20ff85db5602595d2f80e9b9fa
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2025-04-21 18:20:19 +0300

    chore: enhance Babel configuration with module resolver and add font assets for iOS and Android

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset']
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@*': './src/*',
+          '@app': './src/app',
+          '@components': './src/components',
+          '@hooks': './src/lib/hooks',
+          '@core': './src/core',
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@constants': './src/constants',
+          '@utils': './src/utils',
+          '@lib': './src/lib'
+        }
+      }
+    ]
+  ]
 };
