ded5269ce899ac1db3d154ebb3dd4ee7de0e14e2
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2025-05-06 11:58:11 +0300

    tests: add clipboard mock for Jest testing

--- jest/setup.js
+++ jest/setup.js
@@ -26,3 +26,7 @@ jest.mock(
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
 );
+
+jest.mock('@react-native-clipboard/clipboard', () =>
+  require('@react-native-clipboard/clipboard/jest/clipboard-mock')
+);
