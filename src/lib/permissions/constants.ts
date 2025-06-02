8869f78c97af5ef3c4b633b5f02ead8e0708d466
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2025-06-02 11:30:38 +0300

    fix(permissions): update CAMERA permission key to use PermissionKeys enum
    - Refactored the CAMERA permission key in APP_PERMISSIONS to utilize the newly introduced PermissionKeys enum for improved maintainability and clarity in permission management.

--- src/lib/permissions/constants.ts
+++ src/lib/permissions/constants.ts
@@ -1,9 +1,10 @@
 import { Platform } from 'react-native';
 import { PERMISSIONS } from 'react-native-permissions';
+import { PermissionKeys } from './types';
 
-export const APP_SUPPORTED_PERMISSIONS = ['Camera'];
+export const APP_SUPPORTED_PERMISSIONS = [PermissionKeys.CAMERA];
 
 export const APP_PERMISSIONS = {
-  CAMERA:
+  [PermissionKeys.CAMERA]:
     Platform.OS === 'ios' ? PERMISSIONS.IOS.CAMERA : PERMISSIONS.ANDROID.CAMERA
 } as const;
